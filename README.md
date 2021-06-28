3060226c8471b456b9ab0002652da2eeccf00393
Larbi Sahli <larbisahli1905@gmail.com>
2021-06-28 10:35:42 +0100

    adding aws sdk

---
+++ README.md
@@ -0,0 +1,27 @@
+
+## Contents
+
+- [Details about the project](#Details-about-the-project)
+- [Getting Started](#getting-started)
+
+## Details about the project:
+
+- React library.
+- Nextjs framework.
+- Styled-components.
+- PWA using next-offline.
+- SEO using next-seo.
+
+## Getting Started
+
+First, install all dependencies for the project:
+
+```bash
+yarn install
+```
+
+Second, run the development server:
+
+```bash
+yarn dev
+```
--- src/script.js
+++ src/script.js
@@ -3,8 +3,19 @@ import fs from 'fs'
 import axios from 'axios'
 import path from 'path'
 import shortid from 'shortid'
+import AWS from 'aws-sdk';
 
+// import sharp from 'sharp'
 shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$£');
+
+// Set S3 endpoint to DigitalOcean Spaces
+const spacesEndpoint = new AWS.Endpoint(process.env.SPACES_BUCKET_ENDPOINT);
+
+const s3 = new AWS.S3({
+    endpoint: spacesEndpoint,
+    accessKeyId: process.env.SPACES_ACCESS_KEY_ID,
+    secretAccessKey: process.env.SPACES_ACCESS_KEY
+});
 
 if (!fs.existsSync('temp')) {
     fs.mkdirSync('temp');
@@ -25,35 +36,57 @@ const DeleteTempFileImages = () => {
     }
 }
 
-const DownloadImage = async (url, callback = () => void 0) => {
+const Upload = ({ Year, Month, imageFile, filePath }) => {
+
+    const params = {
+        Bucket: process.env.SPACES_BUCKET_NAME,
+        Key: `${Year}/${Month}/${imageFile}`,
+        SourceFile: filePath,
+        ACL: 'public-read',
+        // Metadata: {
+        //     "x-amz-meta-my-key": "your-value"
+        // }
+    };
+
+    s3.putObject(params, function (error, data) {
+        return { error, data }
+    });
+}
+
+export async function DownloadImage(url, imageName, callback = () => void 0) {
 
     if (!url) return { success: false, message: 'url should not be empty' }
 
-    let imageFileName = `NIS_${shortid.generate()}.jpg`
+    const newDate = new Date();
+    const Month = parseInt(newDate.getMonth() + 1)
+    const Year = newDate.getFullYear();
 
-    const filepath = path.join('temp', imageFileName)
+    const DateAsInt = Math.round(newDate.getTime() / 1000) // in seconds
 
-    // const mimetype = 'image/jpeg'
-    // let imageToBeUploaded = { filepath, mimetype }
-
-    // resized => RE
-    // Normal Image size => NIS
+    let imageFile = `${imageName.split(' ').join('_')}_${DateAsInt}_${shortid.generate()}.jpg`
+    const filePath = path.join('temp', imageFile)
 
     if (!/^https/.test(url) || !/^http/.test(url)) {
         var base64Data = url.replace(/^data:image\/png;base64,/, "");
         return new Promise((resolve, reject) => {
-            fs.writeFile(filepath, base64Data, 'base64', function (error) {
-                if (error) {
+            fs.writeFile(filePath, base64Data, 'base64', function (err) {
+                if (err) {
                     DeleteTempFileImages()
-                    reject({ success: false, message: error.message })
+                    reject({ success: false, message: err.message })
                 }
 
-                //  Resize & Upload image to a space
+                // Upload image to digitalOcean space
+                const { error, data } = Upload({ Year, Month, imageFile, filePath })
 
-                // -----------------
+                if (error) {
+                    console.log({ error, stack: error.stack })
+                    return reject({ success: false, message: error.stack })
+                }
+                console.log('===> ', { data })
+
                 callback()
                 DeleteTempFileImages()
-                return resolve({ success: true })
+                return resolve({ success: true, imagePath: `${Year}/${Month}/${imageFile}` })
             })
         })
     }
@@ -64,17 +97,25 @@ const DownloadImage = async (url, callback = () => void 0) => {
     }).then(res =>
         new Promise((resolve, reject) => {
             res.data
-                .pipe(fs.createWriteStream(filepath))
-                .on('finish', () => {
+                .pipe(fs.createWriteStream(filePath))
+                .on('finish', async () => {
 
                     try {
-                        // Resize & Upload image to a space
+
+                        // Upload image to digitalOcean space
+                        const { error, data } = Upload({ Year, Month, imageFile, filePath })
+
+                        if (error) {
+                            console.log({ error, stack: error.stack })
+                            return reject({ success: false, message: error.stack })
+                        }
+                        console.log('===> ', { data })
+                        return resolve({ success: true, imagePath: `${Year}/${Month}/${imageFile}` })
+
                     }
                     catch (error) {
                         return reject({ success: false, message: error.message })
                     }
-
-                    return resolve({ success: true })
                 })
                 .on('error', (error) => reject({ success: false, message: error.message }))
                 .on('close', callback)
@@ -84,16 +125,17 @@ const DownloadImage = async (url, callback = () => void 0) => {
             return { success: false, message: error.message }
         })
         .then((respond) => {
+            console.log(`BOOM`)
             DeleteTempFileImages()
             return respond
         })
 }
 
 
-const url = 'https://ae01.alicdn.com/kf/HTB13gJEKeGSBuNjSspbq6AiipXaM_.jpg'
+const url = 'https://ae01.alicdn.com/kf/HTB13gJEKeGSBuNjSspbq6AiipXaM.jpg'
 
 async function GetImageByUrl(url) {
-    const Results = await DownloadImage(url, () => console.log(`<--- DONE --->`))
+    const Results = await DownloadImage(url, 'product image from ali express', () => console.log(`<--- DONE --->`))
     console.log(`====>`, Results)
 }
 
