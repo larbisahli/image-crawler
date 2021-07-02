843b1d3c388c46acb3a205c996bdc9d663cc8b15
Larbi Sahli <larbisahli1905@gmail.com>
2021-07-02 11:40:24 +0100

    update readme and add promise

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 ## Usage with Next.js
 
+image-crawler will download an image using the URL or DataUrl and then uploads
+the image and a 16px placeholder for Nextjs image component to s3 bucket.
+
 ```javascript
 // server.js
+
+const PrintResults = async () => {
+  const url = 'https://ae01.alicdn.com/kf/HTB13gJEKeGSBuNjSspbq6AiipXaM.jpg';
+  const title = 'product image from ali express';
+
+  const { success, error, image, placeholder } = await UploadImageByUrl(
+    url,
+    title
+  );
+};
+
+PrintResults();
+
+// Results:
+//   {
+//     success: undefined,
+//     error: undefined,
+//     image: {
+//       path: 'temp\\product_image_from_ali_express_1625214253_McTefiJPA.jpg',
+//       ETag: '"fa8bc66b3d45370d5997856fb07cef07"'
+//     },
+//     placeholder: {
+//       path: 'temp\\product_image_from_ali_express_1625214253_McTefiJPA_placeholder.jpg',
+//       ETag: '"22436eaa7cd6c1b0ee25ec171265dcbc"'
+//   }
 ```
+
+How to use the placeholder in Nextjs Image component
 
 ```javascript
 // component.js
@@ -11,7 +41,45 @@ import React from 'react';
 import Image from 'next/image';
 
 const component = () => {
-  return <Image />;
+  // Show something while the placeholder is loading
+  const [Base64Placeholder, setBase64Placeholder] = useState(
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mM8eftXPQAIMgMfS5tX7gAAAABJRU5ErkJggg=='
+  );
+
+  useEffect(() => {
+    // Convert an Image to DataUrl
+    async function fetchData() {
+      const data = await fetch(url);
+      const blob = await data.blob();
+      // eslint-disable-next-line no-undef
+      return await new Promise((resolve) => {
+        const reader = new window.FileReader();
+        reader.readAsDataURL(blob);
+        reader.onloadend = () => {
+          const base64data = reader.result;
+          return resolve(base64data);
+        };
+      }).then((res) => {
+        setBase64Placeholder(res);
+      });
+    }
+
+    if (url) fetchData();
+  }, [url]);
+
+  return (
+    <Image
+      quality={95}
+      width={250}
+      height={250}
+      blurDataURL={Base64Placeholder}
+      placeholder="blur"
+      alt=""
+      className="bg-blue-100 rounded-t"
+      unoptimized={true} // use this untill next.js V11.0.2 is released
+      src="https://bucket-name.fra1.digitaloceanspaces.com/2021/7/product_image_from_ali_express_1625219873_Dpse5Mot9.jpg"
+    />
+  );
 };
 ```
 
---
+++ src/UploadByUrl.js
@@ -0,0 +1,179 @@
+import fs from 'fs';
+import axios from 'axios';
+import path from 'path';
+import shortid from 'shortid';
+import AWS from 'aws-sdk';
+import sharp from 'sharp';
+
+require('dotenv').config();
+
+// import sharp from 'sharp'
+shortid.characters(
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$£'
+);
+
+// Set S3 endpoint to DigitalOcean Spaces
+const spacesEndpoint = new AWS.Endpoint(process.env.SPACES_BUCKET_ENDPOINT);
+
+const s3 = new AWS.S3({
+    endpoint: spacesEndpoint,
+    accessKeyId: process.env.SPACES_ACCESS_KEY_ID,
+    secretAccessKey: process.env.SPACES_ACCESS_SECRET_KEY,
+});
+
+if (!fs.existsSync('temp')) fs.mkdirSync('temp');
+
+const DeleteTempFileImages = async (TemporaryImages) => {
+    try {
+        for await (const file of TemporaryImages) {
+            fs.unlink(path.join('', file), (err) => {
+                if (err) throw err;
+            });
+        }
+    } catch (error) {
+        console.error(`Error:<DeleteTempFileImages> `, { error })
+    }
+};
+
+
+async function UploadImage({ Year, Month, respond }) {
+    const params = [
+        {
+            Bucket: process.env.SPACES_BUCKET_NAME,
+            Key: `${Year}/${Month}/${respond.image.name}`,
+            Body: fs.createReadStream(respond.image.path),
+            ACL: 'public-read',
+        },
+        {
+            Bucket: process.env.SPACES_BUCKET_NAME,
+            Key: `${Year}/${Month}/${respond.placeholder.name}`,
+            Body: fs.createReadStream(respond.placeholder.path),
+            ACL: 'public-read',
+        }
+    ]
+
+    return await Promise.all(params.map(param => s3.putObject(param).promise()))
+}
+
+
+async function UploadImageByUrl(url, title) {
+    if (!url) return { success: false, message: 'url should not be empty' };
+
+    const newDate = new Date();
+    const Month = parseInt(newDate.getMonth() + 1);
+    const Year = newDate.getFullYear();
+    const DateAsInt = Math.round(newDate.getTime() / 1000); // in seconds
+    const imageName = `${title.split(' ').join('_')}_${DateAsInt}_${shortid.generate()}`;
+    const filePath = path.join('temp', `${imageName}.jpg`)
+
+    return new Promise((resolve, reject) => {
+
+        // base64
+        if (!/^https/.test(url) || !/^http/.test(url)) {
+            var base64Data = url.replace(/^data:image\/png;base64,/, '');
+            fs.writeFile(filePath, base64Data, 'base64', async function (error) {
+                if (error) {
+                    return reject({ success: false, error });
+                }
+
+                sharp(filePath)
+                    .resize(16)
+                    .toFile(path.join('temp', `${imageName}_placeholder.jpg`), async (error, info) => {
+                        if (error) {
+                            console.error(`Error:<sharp>`, { error, info });
+                            return reject({ success: false, error });
+                        }
+
+                        return resolve({
+                            success: true,
+                            image: {
+                                name: `${imageName}.jpg`,
+                                path: filePath
+                            },
+                            placeholder: {
+                                name: `${imageName}_placeholder.jpg`,
+                                path: path.join('temp', `${imageName}_placeholder.jpg`)
+                            }
+                        });
+                    });
+            });
+        } else {
+            // url
+            axios({
+                url,
+                responseType: 'stream',
+            }).then(
+                (res) =>
+                    res.data
+                        .pipe(fs.createWriteStream(filePath))
+                        .on('finish', async () => {
+                            sharp(filePath)
+                                .resize(16)
+                                .toFile(path.join('temp', `${imageName}_placeholder.jpg`), async (error, info) => {
+                                    if (error) {
+                                        console.error(`Error:<sharp>`, { error, info });
+                                        return reject({ success: false, error });
+                                    }
+
+                                    return resolve(
+                                        {
+                                            success: true,
+                                            image: {
+                                                name: `${imageName}.jpg`,
+                                                path: filePath
+                                            },
+                                            placeholder: {
+                                                name: `${imageName}_placeholder.jpg`,
+                                                path: path.join('temp', `${imageName}_placeholder.jpg`)
+                                            }
+                                        }
+                                    );
+                                });
+                        })
+                        .on('error', (error) =>
+                            reject({ success: false, error })
+                        )
+                        .on('close', () => void 0))
+        }
+    }).then(async (respond) => {
+
+        const img_res = await UploadImage({ Year, Month, respond });
+
+        respond['image'].ETag = img_res[0].ETag
+        respond['placeholder'].ETag = img_res[1].ETag
+
+        return respond
+
+    }).then((respond) => {
+
+        const { image, placeholder } = respond
+        const arr = [image.path, placeholder.path]
+
+        DeleteTempFileImages(arr)
+
+        return {
+            image: { path: image.path, ETag: image.ETag },
+            placeholder: { path: placeholder.path, ETag: placeholder.ETag }
+        }
+    })
+}
+
+const url = 'https://ae01.alicdn.com/kf/HTB13gJEKeGSBuNjSspbq6AiipXaM.jpg';
+
+const PrintResults = async () => {
+    const {
+        success,
+        error,
+        image,
+        placeholder
+    } = await UploadImageByUrl(url, 'product image from ali express');
+
+    console.log(`====>`, {
+        success,
+        error,
+        image,
+        placeholder
+    });
+};
+
+PrintResults();
